9ca9251d1241ba53bdbcee09ec13fba4da6a34a8
Oleg Zubor <oleg.zubor@mail.ru>
2023-03-24 12:46:26 +0300

    add backend project

---
+++ epolsoft-backend/src/main/java/com/example/epolsoftbackend/EpolsoftBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.epolsoftbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpolsoftBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EpolsoftBackendApplication.class, args);
+    }
+
+}
