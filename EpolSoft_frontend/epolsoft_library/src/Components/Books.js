b4070575e9697e4461b27df56cfc38251ccb2590
Kirill <kirill.bnf@gmail.com>
2023-03-29 15:13:04 +0300

    remove the Input component

--- EpolSoft_frontend/epolsoft_library/src/Components/Books.js
+++ EpolSoft_frontend/epolsoft_library/src/Components/Books.js
@@ -2,7 +2,6 @@ import { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { addNewBook, removeBook, fetchBooks, fetchLocalBooks } from '../store/bookSlice';
 import BookList from './BookList';
-import InputTitle from './InputTitle';
 import Form from "./Form";
 
 function Books() {
@@ -28,11 +27,6 @@ function Books() {
    return (
       <div className='Books'>
          <Form />
-         <InputTitle
-            value={title}
-            updateTitle={setTitle}
-            handleAction={handleAction}
-         />
          {status === 'loading' && <h3>Loading...</h3>}
          {error && <h3>Server error: {error}</h3>}
          <BookList />
