d7a1daec730a83991fc1508c6c8fdeebbc45a2e1
KaffLime <78738107+KaffLime@users.noreply.github.com>
2023-04-03 11:42:39 +0300

    Update FileService.java

--- epolsoft-backend/src/main/java/com/example/epolsoftbackend/services/FileService.java
+++ epolsoft-backend/src/main/java/com/example/epolsoftbackend/services/FileService.java
@@ -1,52 +1,121 @@
-package com.example.epolsoftbackend.services;
+package com.example.epolsoftbackend.controllers;
 
-import org.springframework.stereotype.Service;
+import com.example.epolsoftbackend.entities.Author;
+import com.example.epolsoftbackend.entities.Book;
+import com.example.epolsoftbackend.entities.Library;
+import com.example.epolsoftbackend.entities.Topic;
+import com.example.epolsoftbackend.payload.NoteModel;
+import com.example.epolsoftbackend.services.*;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.StandardCopyOption;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import java.util.List;
+
 import org.springframework.core.io.Resource;
-import org.springframework.core.io.UrlResource;
-import org.springframework.util.StringUtils;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.http.ResponseEntity;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@Service
-public class FileService {
-    private final Path fileStorageLocation = Path.of(System.getProperty("user.home") + "/bookCollection");
-    
-    public String storeFile(MultipartFile file) throws IOException {
-        String fileName = StringUtils.cleanPath(file.getOriginalFilename());
+import javax.servlet.http.HttpServletRequest;
 
-        Files.copy(file.getInputStream(),
-                Path.of(fileStorageLocation.toString() + "/" + fileName),
-                StandardCopyOption.REPLACE_EXISTING);
+@RestController
+@RequestMapping("/library")
+public class LibraryController {
+    final FileService fileService;
+    final BookService bookService;
+    final AuthorService authorService;
+    final TopicService topicService;
+    final LibraryService libraryService;
 
-        return fileName;
+    public LibraryController(FileService fileService, BookService bookService, AuthorService authorService, TopicService topicService, LibraryService libraryService) {
+        this.fileService = fileService;
+        this.bookService = bookService;
+        this.authorService = authorService;
+        this.topicService = topicService;
+        this.libraryService = libraryService;
+    }
+
+
+    @PostMapping("/uploadFile")
+    @ResponseStatus(HttpStatus.OK)
+    public void uploadFile(@RequestParam("file") MultipartFile file) throws IOException {
+        fileService.storeFile(file);
     }
     
-    public Resource loadFileAsResource(String fileName) throws MalformedURLException {
-        Path filePath = this.fileStorageLocation.resolve(fileName).normalize();
-        Resource resource = new UrlResource(filePath.toUri());
-        
-        if(resource.exists()) {
-            return resource;
-        } else {
-            return null;
+    @PostMapping("/deleteFile")
+    @ResponseStatus(HttpStatus.OK)
+    public void deleteFile(@RequestParam("fileName") String fileName) throws IOException {
+        fileService.deleteFile(fileName);
+    }
+    
+    @GetMapping("/downloadFile/{fileName:.+}")
+    public ResponseEntity<Resource> downloadFile(@PathVariable String fileName, HttpServletRequest request) throws MalformedURLException {
+        // Load file as Resource
+        Resource resource = fileService.loadFileAsResource(fileName);
+
+        // Try to determine file's content type
+        String contentType = null;
+        try {
+            contentType = request.getServletContext().getMimeType(resource.getFile().getAbsolutePath());
+        } catch (IOException ex) {
+            //logger.info("Could not determine file type.");
         }
+
+        // Fallback to the default content type if type could not be determined
+        if(contentType == null) {
+            contentType = "application/octet-stream";
+        }
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(contentType))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
+                .body(resource);
     }
-    
-    public boolean deleteFile(String fileName) throws IOException {
-        Files.deleteIfExists(Path.of(fileStorageLocation + "/" + fileName));
-        
-        return Files.exists(Path.of(fileStorageLocation + "/" + fileName));
+
+    //CRUD data operations
+    @PostMapping()
+    public ResponseEntity<Book> createNote(@RequestBody NoteModel newNote){
+        var topic = topicService.searchExistingOrCreateNew(newNote.getTopicId(), newNote.getTopicName());
+        var newAuthor = authorService.create(newNote.getAuthorName());
+        return bookService.create(newNote, newAuthor, topic);
     }
+
+    @GetMapping()
+    public ResponseEntity<List<Library>> getBooks(@RequestParam(value = "numberPage", defaultValue = "1") int numberPage,
+                                                  @RequestParam(value = "sortingOrder") String sortingOrder,
+                                                  @RequestParam(value = "size", defaultValue = "10") int size,
+                                                  @RequestParam(value = "sortingField", defaultValue = "name") String sortingField){
+        Pageable pageable = PageRequest.of(numberPage - 1, size);
+        return new ResponseEntity<>(libraryService.findByCriteria(null, sortingOrder, sortingField,
+                pageable), HttpStatus.OK);
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<Book> updateOneBook(@PathVariable("id") long id, @RequestBody NoteModel updateNote) {
+        Author author = authorService.searchExistingOrCreateNew(updateNote.getAuthorId(), updateNote.getAuthorName());
+        Topic topic = topicService.searchExistingOrCreateNew(updateNote.getTopicId(), updateNote.getTopicName());
+        return bookService.updateById(id, updateNote, author, topic);
+    }
+
+    @GetMapping("/get/authors")
+    public ResponseEntity<List<Author>> getAllAuthors(){
+        return authorService.getAll();
+    }
+    @GetMapping("/get/topics")
+    public ResponseEntity<List<Topic>> getAllTopics(){
+        return topicService.getAll();
+    }
+
+    @DeleteMapping("/deleteNote/{id}")
+    public ResponseEntity<HttpStatus> deleteNote(@PathVariable("id") long id) {
+            return bookService.deleteById(id);
+    }
+
 }
