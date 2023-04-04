12206f06b82e0d3423f236860885cd3bf77a1aa5
KaffLime <78738107+KaffLime@users.noreply.github.com>
2023-04-04 15:09:52 +0300

    Update 03-init-tables.sql

--- init-db-sql-scripts/03-init-tables.sql
+++ init-db-sql-scripts/03-init-tables.sql
@@ -1,37 +1,42 @@
 CREATE TABLE IF NOT EXISTS public.role
 (
     id bigint NOT NULL,
-    name character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    name VARCHAR(255) COLLATE pg_catalog."default" NOT NULL,
 );
 
-ALTER TABLE public.author
-    ADD COLUMN created_at TIMESTAMP WITHOUT TIME ZONE,
-    ADD COLUMN updated_at TIMESTAMP WITHOUT TIME ZONE,
-    ADD COLUMN isBlocked boolean NOT NULL,
-    ADD COLUMN mail citext NOT NULL UNIQUE,
-    ADD COLUMN avatar VARCHAR(255) COLLATE pg_catalog."default" NOT NULL,
-    ADD COLUMN password_hash VARCHAR(255) COLLATE pg_catalog."default" NOT NULL;
+ALTER TABLE public.user
+    ADD COLUMN IF NOT EXISTS created_at TIMESTAMP WITHOUT TIME ZONE,
+    ADD COLUMN IF NOT EXISTS updated_at TIMESTAMP WITHOUT TIME ZONE,
+    ADD COLUMN IF NOT EXISTS isBlocked boolean NOT NULL,
+    ADD COLUMN IF NOT EXISTS mail citext NOT NULL UNIQUE,
+    ADD COLUMN IF NOT EXISTS avatar VARCHAR(255) COLLATE pg_catalog."default" NOT NULL,
+    ADD COLUMN IF NOT EXISTS password_hash VARCHAR(255) COLLATE pg_catalog."default" NOT NULL;
 
-CREATE TABLE IF NOT EXISTS public.author_role
+CREATE TABLE IF NOT EXISTS public.user_role
 (
-    author_id BIGINT NOT NULL,
+    user_id BIGINT NOT NULL,
     role_id BIGINT NOT NULL,
-    CONSTRAINT author_role_pkey PRIMARY KEY (author_id, role_id),
-    CONSTRAINT author_role_to_author_fkey FOREIGN KEY (author_id)
-        REFERENCES public.author (id) MATCH SIMPLE
+    CONSTRAINT user_role_pkey PRIMARY KEY (user_id, role_id),
+    CONSTRAINT user_role_to_user_fkey FOREIGN KEY (user_id)
+        REFERENCES public.user (id) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION,
-    CONSTRAINT author_role_to_role_fkey FOREIGN KEY (role_id)
+    CONSTRAINT user_role_to_role_fkey FOREIGN KEY (role_id)
         REFERENCES public.role (id) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
 );
 
 ALTER TABLE public.topic
-    ADD COLUMN created_at TIMESTAMP WITHOUT TIME ZONE,
-    ADD COLUMN updated_at TIMESTAMP WITHOUT TIME ZONE,
-    ADD COLUMN isActive BOOLEAN NOT NULL;
+    ADD COLUMN IF NOT EXISTS created_at TIMESTAMP WITHOUT TIME ZONE,
+    ADD COLUMN IF NOT EXISTS updated_at TIMESTAMP WITHOUT TIME ZONE,
+    ADD COLUMN IF NOT EXISTS isActive BOOLEAN NOT NULL;
 
 INSERT INTO public.role (id, name) VALUES (1, "USER"),
                                           (2, "ADMIN"),
                                           (3, "ANONYMOUS");
+
+ALTER TABLE public.book
+    DROP IF EXISTS COLUMN file,
+    ADD COLUMN IF NOT EXISTS fileName VARCHAR(255) COLLATE pg_catalog."default" NOT NULL,
+    ADD COLUMN IF NOT EXISTS filePath VARCHAR(255) COLLATE pg_catalog."default" NOT NULL;
