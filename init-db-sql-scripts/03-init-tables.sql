f58b3d243eeb5585bc6178867ba8297a0adc6305
KaffLime <78738107+KaffLime@users.noreply.github.com>
2023-04-04 11:54:21 +0300

    Update 03-init-tables.sql

--- init-db-sql-scripts/03-init-tables.sql
+++ init-db-sql-scripts/03-init-tables.sql
@@ -5,19 +5,18 @@ CREATE TABLE IF NOT EXISTS public.role
 );
 
 ALTER TABLE public.author
-    ADD COLUMN created_at timestamp without time zone,
-    ADD COLUMN updated_at timestamp without time zone,
+    ADD COLUMN created_at TIMESTAMP WITHOUT TIME ZONE,
+    ADD COLUMN updated_at TIMESTAMP WITHOUT TIME ZONE,
     ADD COLUMN isBlocked boolean NOT NULL,
     ADD COLUMN mail citext NOT NULL UNIQUE,
-    ADD COLUMN avatar character varying(255) COLLATE pg_catalog."default" NOT NULL,
-    ADD COLUMN password_hash character varying(255) COLLATE pg_catalog."default" NOT NULL;
+    ADD COLUMN avatar VARCHAR(255) COLLATE pg_catalog."default" NOT NULL,
+    ADD COLUMN password_hash VARCHAR(255) COLLATE pg_catalog."default" NOT NULL;
 
 CREATE TABLE IF NOT EXISTS public.author_role
 (
-    id bigint NOT NULL,
-    author_id bigint NOT NULL,
-    role_id bigint NOT NULL,
-    CONSTRAINT author_role_pkey PRIMARY KEY (id),
+    author_id BIGINT NOT NULL,
+    role_id BIGINT NOT NULL,
+    CONSTRAINT author_role_pkey PRIMARY KEY (author_id, role_id),
     CONSTRAINT author_role_to_author_fkey FOREIGN KEY (author_id)
         REFERENCES public.author (id) MATCH SIMPLE
         ON UPDATE NO ACTION
@@ -29,9 +28,10 @@ CREATE TABLE IF NOT EXISTS public.author_role
 );
 
 ALTER TABLE public.topic
-    ADD COLUMN created_at timestamp without time zone,
-    ADD COLUMN updated_at timestamp without time zone,
-    ADD COLUMN isActive boolean NOT NULL;
+    ADD COLUMN created_at TIMESTAMP WITHOUT TIME ZONE,
+    ADD COLUMN updated_at TIMESTAMP WITHOUT TIME ZONE,
+    ADD COLUMN isActive BOOLEAN NOT NULL;
 
 INSERT INTO public.role (id, name) VALUES (1, "USER"),
-                                          (2, "ADMIN");
+                                          (2, "ADMIN"),
+                                          (3, "ANONYMOUS");
