24b6ffdc09d47998a8f37149a2f9c394e5f138ad
carpeat <carpeat@mail.ru>
2023-04-04 10:24:27 +0300

    changed

--- epolsoft-backend/src/main/java/com/example/epolsoftbackend/author/AuthorRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.epolsoftbackend.author;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuthorRepository extends JpaRepository<Author, Long> {
-}
--- epolsoft-backend/src/main/java/com/example/epolsoftbackend/book/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.epolsoftbackend.book;
-
-import com.example.epolsoftbackend.book.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BookRepository extends JpaRepository<Book, Long> {
-}
