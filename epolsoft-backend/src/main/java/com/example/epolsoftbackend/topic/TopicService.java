2c5d9476f241b46a880c9b81b4a701050418e6c1
Oleg Zubor <oleg.zubor@mail.ru>
2023-04-06 21:49:48 +0300

    Added method in controller to get available topics and realizations this methods in service

--- epolsoft-backend/src/main/java/com/example/epolsoftbackend/topic/TopicService.java
+++ epolsoft-backend/src/main/java/com/example/epolsoftbackend/topic/TopicService.java
@@ -1,6 +1,8 @@
 package com.example.epolsoftbackend.topic;
 
+import com.example.epolsoftbackend.topic.DTO.TopicCreateDTO;
 import com.example.epolsoftbackend.topic.DTO.TopicResponseDTO;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 
 import java.util.List;
@@ -9,6 +11,8 @@ import java.util.Optional;
 public interface TopicService {
 
     Optional<Topic> findById(Long id);
-    ResponseEntity<List<TopicResponseDTO>> getAllTopics();
+    ResponseEntity<List<TopicResponseDTO>> getAllAvailableTopics();
+    ResponseEntity<List> getAllTopics(Pageable pageable);
+    ResponseEntity<TopicResponseDTO> createTopic(TopicCreateDTO topicCreateDTO);
 
 }
