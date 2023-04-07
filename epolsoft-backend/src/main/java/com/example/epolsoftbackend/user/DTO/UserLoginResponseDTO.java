493e9e9108cb0f26f7ac36fc91ff5ac22b743789
Oleg Zubor <oleg.zubor@mail.ru>
2023-04-07 09:55:04 +0300

    add dto user

---
+++ epolsoft-backend/src/main/java/com/example/epolsoftbackend/user/DTO/UserLoginResponseDTO.java
@@ -0,0 +1,32 @@
+package com.example.epolsoftbackend.user.DTO;
+
+import com.example.epolsoftbackend.user_role.UserRole;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+
+import java.util.Set;
+
+@Builder
+@ToString
+@Getter
+@Setter
+@Data
+public class UserLoginResponseDTO {
+    @JsonProperty("id")
+    private long id;
+
+    @JsonProperty("name")
+    private String name;
+
+    @JsonProperty("mail")
+    private String mail;
+
+    @JsonProperty("isBlocked")
+    private boolean isBlocked;
+
+    @JsonProperty("roles")
+    private Set<UserRole> roles;
+
+    @JsonProperty("token")
+    private String token;
+}
