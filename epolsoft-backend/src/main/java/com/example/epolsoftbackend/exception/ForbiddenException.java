5a1a507e793738f50e3275e51da8d1f004e2d490
KaffLime <artem.korolchuk.02@mail.ru>
2023-04-09 00:56:06 +0300

    Added file exception processing

---
+++ epolsoft-backend/src/main/java/com/example/epolsoftbackend/exception/ForbiddenException.java
@@ -0,0 +1,28 @@
+package com.example.epolsoftbackend.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@Getter
+@ResponseStatus(value = HttpStatus.FORBIDDEN)
+public class ForbiddenException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    private ApiResponse apiResponse;
+
+    public ForbiddenException(ApiResponse apiResponse) {
+        super();
+        this.apiResponse = apiResponse;
+    }
+
+    public ForbiddenException(String message) {
+        super(message);
+    }
+
+    public ForbiddenException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+}
---
+++ epolsoft-backend/src/main/java/com/example/epolsoftbackend/exception/RestControllerExceptionHandler.java
@@ -0,0 +1,44 @@
+package com.example.epolsoftbackend.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@ControllerAdvice
+public class RestControllerExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    @ResponseBody
+    public ResponseEntity<ApiResponse> resolveException(ResourceNotFoundException exception) {
+        ApiResponse apiResponse = exception.getApiResponse();
+
+        return new ResponseEntity<>(apiResponse, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(BadRequestException.class)
+    @ResponseBody
+    public ResponseEntity<ApiResponse> resolveException(BadRequestException exception) {
+        ApiResponse apiResponse = exception.getApiResponse();
+
+        return new ResponseEntity<>(apiResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(ForbiddenException.class)
+    @ResponseBody
+    public ResponseEntity<ApiResponse> resolveException(ForbiddenException exception) {
+        ApiResponse apiResponse = exception.getApiResponse();
+
+        return new ResponseEntity<>(apiResponse, HttpStatus.FORBIDDEN);
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    @ResponseBody
+    public ResponseEntity<ApiResponse> resolveAllUncaughtException(RuntimeException exception) {
+        ApiResponse apiResponse = new ApiResponse(false, exception.getMessage());
+
+        return new ResponseEntity<>(apiResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+}
