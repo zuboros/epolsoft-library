09889d1366debfa3bc9139d4a3bd3c07e28f9dc0
carpeat <carpeat@mail.ru>
2023-04-18 10:58:13 +0300

    fixes and validation elements

--- epolsoft-backend/src/main/java/com/example/epolsoftbackend/user/DTO/UserLoginDTO.java
+++ epolsoft-backend/src/main/java/com/example/epolsoftbackend/user/DTO/UserLoginDTO.java
@@ -1,6 +1,5 @@
 package com.example.epolsoftbackend.user.DTO;
 
-import com.example.epolsoftbackend.validation.PasswordConstraint;
 import lombok.*;
 
 import javax.validation.constraints.Email;
@@ -14,7 +13,6 @@ public class UserLoginDTO {
     @Email
     private String mail;
 
-    @PasswordConstraint
     private String password;
 
 }
